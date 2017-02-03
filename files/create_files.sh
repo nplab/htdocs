9b6b30a6268fa8e51c67afba9586faddb203754f
Felix Weinrank <weinrank@fh-muenster.de>
2017-02-03 18:29:07 +0100

    all better

---
+++ files/create_files.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+truncate -s 512B 512B
+truncate -s 1K 1K
+truncate -s 2K 2K
+truncate -s 4K 4K
+truncate -s 1M 1M
+truncate -s 2M 2M
+truncate -s 4M 4M
+truncate -s 8M 8M
+truncate -s 16M 16M
+truncate -s 32M 32M
+truncate -s 64M 64M
+truncate -s 128M 128M
+truncate -s 1G 1G
